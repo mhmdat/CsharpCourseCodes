29f7d6bc3d731526216428b7a3f3ff61b162aaa9
Mohammad Atiyeh <alhachemm@gmail.com>
2017-09-30 14:20:52 +0300

    Increments-decrements
    C-Charp Course Increments-Decrements

---
+++ Increments-Decrements/Increments-Decrements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increments_Decrements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Var1 = 5;
+            int temp1 = Var1++;
+
+           
+
+            Console.WriteLine("{0}", temp1);
+            
+        
+
+
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
