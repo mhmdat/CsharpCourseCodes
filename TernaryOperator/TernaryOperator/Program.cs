2727065aa784cff60abcf32c57bb0da2adf6cb03
Mohammad Atiyeh <alhachemm@gmail.com>
2017-09-30 14:27:40 +0300

    TernaryOperator
    C-Charp Course Ternary Operator

---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  int myVar = 7;
+
+            //string myResult = (myVar < 10) ? "Less than 10" : "Greater than 10";
+            //Console.WriteLine("My Variable Result is {0}", myResult);
+
+            string myName = "Mhmd", result;
+            result = (myName.Equals("Mhmd")) ? "Yes" : "No";
+            Console.WriteLine("My Variable result is {0}", result);
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
