ea2669027cdfde6f78db4fa2fb4ca5daaaa40c3b
Mohammad Atiyeh <alhachemm@gmail.com>
2017-09-30 14:05:25 +0300

    Expressions
    C-Charp Course-Expressions

---
+++ Expressions/Expressions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 10;
+            int myVar2 = 15;
+
+
+            int temp = myVar1 + myVar2;
+            Console.WriteLine("The result is {0}", temp);
+            Console.ReadKey();
+
+
+        }
+    }
+}
