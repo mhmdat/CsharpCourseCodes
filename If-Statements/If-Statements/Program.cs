d7a4cfb085da2c2b6cbb750470f485f513f7ffe8
Mohammad Atiyeh <alhachemm@gmail.com>
2017-09-30 14:19:55 +0300

    If-Statements
    C-Charp Course IfStatements

---
+++ If-Statements/If-Statements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*int myVar = 10;
+           
+            if (myVar == 10)
+            {
+                 myVar = 11;
+                Console.WriteLine("{0}", myVar);
+                Console.WriteLine("My Variable is equal 10");
+
+            }
+            else
+                Console.WriteLine("My Variable is not equal 10");
+            */
+            int myVar1 = 9, myVar2 = 7;
+            if ((myVar1 <= 7) || (myVar2 >= 10))
+            {
+                Console.WriteLine("The Condition is True");
+            }
+                else
+            {
+                Console.WriteLine("My Variables Conditions Is False");
+
+            }
+
+            Console.ReadKey();
+            
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
