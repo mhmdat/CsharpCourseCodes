8d5deb2e222618956eb875839ca9508d34abb1b4
Mohammad Atiyeh <alhachemm@gmail.com>
2017-09-30 14:07:40 +0300

    FloatTypes
    C-Charp FloatTypes

---
+++ FloatType/FloatType/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flMin = float.MinValue;
+            float flMax = float.MaxValue;
+
+            Console.WriteLine("Min value equal (0) Max val equal (1)", flMin, flMax);
+
+            float flVar = 1F / 3;
+            double dlVar = 1D / 3;
+            decimal dcVar = 1M / 3;
+
+            Console.WriteLine("\nfloatvalue = (0)\ndouble value = (1)\ndecimal value = (3)", flVar, dlVar, dcVar);
+            Console.ReadKey();
+
+
+             
+
+        
+       
