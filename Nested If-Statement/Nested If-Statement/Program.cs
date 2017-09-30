909943b9f83b7734c5631d2ecce4a1a599e11134
Mohammad Atiyeh <alhachemm@gmail.com>
2017-09-30 14:24:01 +0300

    Nested If-Statement
    C-Charp Course Nested If-Statement

---
+++ Nested If-Statement/Nested If-Statement/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_If_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 10;
+
+           String myResult;
+            if (myVar1 < 10)
+            {
+                myResult = "Less than 10";
+                Console.WriteLine(myResult);
+
+            }
+            if (myVar1 > 10)
+            {
+                myResult = "Greater than 10";
+                Console.WriteLine(myResult);
+            }
+
+            if (myVar1 == 10) ;
+            {
+                myResult = "Equal 10";
+                Console.WriteLine(myResult);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
+            
+        
+        
+
+      
+        
+          
+
+
