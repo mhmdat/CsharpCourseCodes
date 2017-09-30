fa975870652f7a4d6a27313428fc5fd7787daaab
Mohammad Atiyeh <alhachemm@gmail.com>
2017-09-30 14:26:06 +0300

    StringType
    C-Charp Course StringType

---
+++ StringType/StringType/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring = "Mhmd said this is a good course, and I think";
+            string mystring2 = @"yes it is";
+            
+                
+            string mystring3 = mystring + mystring2;
+
+
+            Console.WriteLine("{0}", mystring3);
+            Console.ReadKey();
+        }
+    }
+}
