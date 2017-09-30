0cfb691382b77a87673c86436dcd10c61a156929
Mohammad Atiyeh <alhachemm@gmail.com>
2017-09-30 14:21:41 +0300

    Increments-Decrements2
    C-Charp Course Increments-Decrements 2

---
+++ IncrementsDecrements2/IncrementsDecrements2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementsDecrements2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar1 = 7;
+            int myVar2 = 2;
+
+            int temp = myVar1-- / ++myVar2;
+            Console.WriteLine("{0}", temp);
+
+            int MyVar1 = myVar1--;
+            int MVar2 = ++myVar2;
+
+            Console.WriteLine("{0} {1}", MyVar1, MVar2);
+            
+
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
