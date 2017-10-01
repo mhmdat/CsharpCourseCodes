5e168c24caaafbf3b5fc4c8d4954612903f5d89e
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-01 10:12:11 +0300

    Do-While
    C-Charp Course Do-While

---
+++ Do-While/Do-While/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mySalary = 1000.2, total = 0.0;
+            int i = 0;
+
+            do
+            {
+                total += mySalary;
+                i++;
+                Console.WriteLine("i Values equal {0}", i);
+
+            } while (i < 10);
+            Console.WriteLine("Salary Values equal {0}", total);
+            Console.ReadKey();
+       
+        }
+    }
+}
