190c5b54f829be33392e56ae289742a9448949eb
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-03 10:05:56 +0300

    NestedLoop
    C-Charp Course Nested Loop

---
+++ NestedLoo/NestedLoo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Hell Mhmd");
+                for(int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(" \t Hello World");
+                }
+                Console.WriteLine("\n");
+                
+            }
+                Console.ReadKey();
+        }
+    }
+}
