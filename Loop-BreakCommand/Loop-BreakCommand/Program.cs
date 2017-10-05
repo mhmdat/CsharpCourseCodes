37495fb0cafd04a00b4806a86088191715ade64e
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-05 12:45:40 +0300

    Loop-BreakCommand
    C-Sharp Course Loop Break Command

---
+++ Loop-BreakCommand/Loop-BreakCommand/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_BreakCommand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hello Mhmd");
+                if (i > 2)
+                    continue;
+                for (int j = 0; j <= 4; j++)
+                {
+                    Console.WriteLine("\t Hello World");
+                }
+                Console.WriteLine("\n");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
