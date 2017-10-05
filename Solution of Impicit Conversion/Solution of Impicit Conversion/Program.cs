42207797a2157adc5d5aa75e3a51eaa68308eb0b
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-05 13:18:50 +0300

    Impicit Conversion
    C-Sharp Course Impicit Conversion

---
+++ Solution of Impicit Conversion/Solution of Impicit Conversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_of_Impicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myVar = 1000;
+            float myVar2 = myVar;
+            Console.WriteLine($"{myVar2}");
+            char myVar3 = 'G';
+            int myIntVar = myVar3;
+            Console.ReadKey();
+                
+        }
+    }
+}
