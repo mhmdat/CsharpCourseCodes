365a3e5e955b70319d604ed4c3ac8f6e4b82ff73
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-18 04:02:07 +0300

    Checked-Unchecked
    C-Sharp Course Checked and Unchecked

---
+++ Checked-Unchecked/Checked-Unchecked/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checked_Unchecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte mybytetype;
+            int myint = 350;
+            //mybytetype = checked((byte)myint);
+            mybytetype = unchecked((byte)myint);
+            Console.WriteLine($"My Variable Int = {myint}" + "\n");
+            Console.WriteLine($"My byte Variable = {mybytetype}");
+            Console.ReadKey();
+        }
+    }
+}
