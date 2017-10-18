c5290921a1a0deeb08ca1ea8ba1a9807c5e1fa86
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-18 03:47:03 +0300

    Explicit-Conversion
    C-Sharp Course Explicit conversion

---
+++ Explicit-Conversion/Explicit-Conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type1 = (Type1)Type2                                 //Casting
+            byte mybytetype;
+            int myint = 17;
+            mybytetype = (byte)myint;
+            Console.WriteLine($"My Variable int = {myint}" + "\n");
+            Console.WriteLine($"My Byte Variable = {mybytetype}");
+            Console.ReadKey();
+            
+        }
+    }
+}
