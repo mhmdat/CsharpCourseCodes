61284af7bb374be33b5a4c39e69d28419496be70
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-19 02:45:08 +0300

    Solution18

---
+++ Solutions/Solution 18/Solution 18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myfloatvar = 45.56f;
+            string mystringvar, mysubstring;
+            mystringvar = Convert.ToString(myfloatvar);
+            mysubstring = mystringvar.Substring(3,4);
+            Console.WriteLine($"My Variable is {myfloatvar} \n");
+            Console.WriteLine("My String Variable is {0} \n", mystringvar);
+            Console.WriteLine("My String Variable is {0} \n", mysubstring);
+            Console.ReadKey();
+        }
+    }
+}
