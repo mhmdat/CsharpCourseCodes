4a4bb3f885ff993ffdafb72fd2f578992ca02628
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-19 02:33:01 +0300

    Explicit Conversion Using Convert Command
    C-Sharp Course Explicit Conversion Using Convert Command

---
+++ Explicit Conversion using Conver Command/Explicit Conversion using Conver Command/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Conversion_using_Conver_Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mydoublevar = 2500.45D;
+            string mystringvar, mysubstring;
+            mystringvar = Convert.ToString(mydoublevar);
+            mysubstring = mystringvar.Substring(0, 2);
+            Console.WriteLine($"My Variable is {mydoublevar} \n");
+            Console.WriteLine("My String Variable is {0} \n", mystringvar);
+            Console.WriteLine("My String Variable is {0} \n", mysubstring);
+            Console.ReadKey();
+        }
+    }
+}
