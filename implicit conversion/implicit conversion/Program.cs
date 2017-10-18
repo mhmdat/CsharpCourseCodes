c94d7bd1926ec425ef4f4fe386d5d1d40e003c87
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-18 03:13:31 +0300

    Imlicit-Conversion
    C-Sharp Course Implicit Conversion

---
+++ implicit conversion/implicit conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace implicit_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long myvar1 = 2000;
+            float myvar2 = myvar1;
+            //Console.WriteLine($"{myvar2}");
+            char myvar3 = 'N';
+            int myvar4 = myvar3;
+            Console.WriteLine($"{myvar4}");
+        
+            Console.ReadKey();
+        }
+    }
+}
