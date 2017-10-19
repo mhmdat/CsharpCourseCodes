27d10f283da78057d4df5590d67c0ec04f870dc2
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-20 02:53:27 +0300

    Challange 20

---
+++ Solutions/Challange 20/Challange 20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange_20
+{
+    class Program
+    {
+        enum orientations :sbyte
+        {
+            north = 2,
+            south = 3,
+            west = 4,
+            east = 5
+            
+            
+        }
+        static void Main(string[] args)
+        {
+            orientations Anorth = orientations.north;
+            Console.WriteLine($"The First Orientation is {Anorth}" + "\n");
+            orientations Asouth = orientations.south;
+            Console.WriteLine($"The Second Orientation is {Asouth}" + "\n");
+            orientations Awest = orientations.west;
+            Console.WriteLine($"The Third Orientation is {Awest}" + "\n");
+            orientations Aeast = orientations.east;
+            Console.WriteLine($"The Forth Orientation is {Aeast}" + "\n");
+            Console.ReadKey();
+               
+
+
+        }
+    }
+}
