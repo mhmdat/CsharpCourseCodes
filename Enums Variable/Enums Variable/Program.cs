ea8067390b1c166b44b2ed6f2643e5e4a9ceca60
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-20 02:33:40 +0300

    Enums Variable
    C-Sharp Course Enums Variable

---
+++ Enums Variable/Enums Variable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_Variable
+{
+    class Program
+    {
+        enum humansex :sbyte
+        {
+            male = 1,
+            female = 2
+        }
+        static void Main(string[] args)
+        {
+            sbyte humansbyte;
+            string humangender;
+            humansex myhumansex = humansex.male;
+            Console.WriteLine($"Human Sex = {myhumansex}" + "\n");
+            humansbyte = (sbyte)myhumansex;
+            humangender = Convert.ToString(myhumansex);
+            Console.WriteLine($"Human Gender Index = {humansbyte}" + "\n");
+            Console.WriteLine($"Human Associated With This Index = {humangender}" + "\n");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
