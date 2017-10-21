372a23f4f02d9d643d867b42634f812320da8b14
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-22 00:58:51 +0300

    Array And ForLoop
    C-Sharp Course Array and ForLoop

---
+++ Array And ForLoop/Array And ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_And_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myArraysize = 5;
+            int [] myNumbers = { 1, 5, 8, 9, 7 };
+            int[] myNumbers2 = new int[5];
+            int[] myNumber3 = new int [myArraysize] {1, 5, 8, 9, 7};
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine($"My Numbers" + (i + 1) + "=" + $"{myNumbers[i]}" + "");
+            Console.ReadKey();
+        }
+    }
+}
