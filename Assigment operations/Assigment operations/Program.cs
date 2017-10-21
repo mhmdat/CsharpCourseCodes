b281d88870bcd4314da7f621f4f9d4406ad08d33
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-22 01:01:41 +0300

    Assigment Operation
    C-Sharp Course Assigment Operation

---
+++ Assigment operations/Assigment operations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //= \+=\ *= \ /= \%=
+
+            int myVar1 = 7,  myVar2 = 3;
+
+            //myVar2 = myVar1;
+
+            //myVar1 += myVar2;   // myVar1 = myVar1 + myVar2
+
+            //myVar1 -= myVar2;    //myvar1 = myVar1 - myVar2
+
+            //myVar1 *= myVar2;      //myVar1 = myVar1 * myVar2
+
+            //myVar1 /= myVar2;      //myVar1 = myVar1 / myVar2
+
+            //myVar1 %= myVar2;      //myVar1 = myVar1 %= myVar2
+
+          
+
+            Console.WriteLine("The result is = {0} The values is = {1}", myVar1, myVar2);
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
