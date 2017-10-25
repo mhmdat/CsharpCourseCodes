09691b8bef303043ad2fa2669edb7f3c197dbf20
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-25 07:25:49 +0300

    ForLoop VS Foreach in Array
    C-Sharp Course ForLoop VS Foreach in Array

---
+++ ForLoop Vs Foreach in Array/ForLoop Vs Foreach in Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop_Vs_Foreach_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFullName = { "Mhmd", "Kamel", "Atiyeh" };
+            int myArrayLength = myFullName.Length;
+            for (int i = 0; i < myArrayLength; i++)
+            {
+                myFullName[2] = "Yousif";
+               Console.WriteLine($"{myFullName}" + "");
+
+            }
+            Console.WriteLine("\n My Array Length Is" +" " + $"{myArrayLength}");
+            foreach (string myname in myFullName)
+            {
+               
+                Console.WriteLine($"{myname}" + "");
+            }
+            Console.ReadKey();
+        }
+    }
+}
