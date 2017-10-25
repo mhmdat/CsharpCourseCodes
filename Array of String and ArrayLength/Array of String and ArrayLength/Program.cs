c55b1717a8d3ce94906bc63e707c188931bf701a
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-25 07:07:58 +0300

    Array of String and ArrayLength
    C-Sharp Course Array of String and ArrayLength

---
+++ Array of String and ArrayLength/Array of String and ArrayLength/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_of_String_and_ArrayLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFullName ={ "Mhmd", "Kamel", "Atiyeh" };
+            int myArrayLength = myFullName.Length;
+            for (int i = 0; i < myArrayLength; i++) 
+            {
+                Console.WriteLine($"{myFullName}" + "");
+
+            }
+            Console.WriteLine("\n My Array Length Is" + $"{myArrayLength}");
+            Console.ReadKey();
+        }
+    }
+}
