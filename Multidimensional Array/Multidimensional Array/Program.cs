3d73e377fdd6cd6790e87e2a6dd01c21cb0baed4
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-26 03:11:09 +0300

    Multidimensional Array
    C-Sharp Couse Multidimensional Array

---
+++ Multidimensional Array/Multidimensional Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multidimensional_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    int[,] myNumbers = {
+
+        //        {1, 2, 3, 4},
+        //        {1, 3, 4, 5 },
+        //       {3, 4, 5, 6 }
+
+        //    };
+        //    for (int i = 0; i < 3; i++)
+        //    {
+        //        for (int j = 0; j < 4; j++)
+
+        //        {
+        //            Console.Write("{0}", $"{myNumbers[i, j]}" + " ");
+        //        }
+        //        Console.WriteLine(" ");
+        //    }
+            int[,,] myArray3Da = new int[2, 2, 3]
+            {
+                {
+                             { 1, 2, 3 }, { 4, 5, 6}
+                },
+
+                {
+                    { 7, 8, 9 }, { 10, 11, 12}
+
+                }
+            };
+            Console.Write("{0}", $"{myArray3Da}");
+            Console.ReadKey();
+        
+
+            
+        }
+    }
+}
