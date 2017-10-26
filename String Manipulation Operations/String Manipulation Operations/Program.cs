263f058f192598217ac1bc5d6031e5840c6d14ad
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-27 02:14:29 +0300

    String Manibulation Operations
    C-Sharp Course String Manibulation Operations

---
+++ String Manipulation Operations/String Manipulation Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manipulation_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring = "Hello My Friends";
+            char[] mychararray = mystring.ToCharArray();
+            Console.WriteLine($"The String Length is {mystring.Length} \n");
+                foreach (char mychar in mychararray)
+            {
+                Console.WriteLine($"{mychar}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
