fae1436cff28d60aeac2cd7cf6d8be2fe2d35558
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-27 02:25:01 +0300

    Challange 26

---
+++ Solutions/Challange 26/Challange 26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mystring = { "I", "am", "smart" };
+            int myArrlength = mystring.Length;
+            for (int i = 0; i < myArrlength; i++) 
+            {
+                mystring[0] = "He";
+                mystring[1] = "is";
+                Console.Write($"{mystring[i]}" + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
