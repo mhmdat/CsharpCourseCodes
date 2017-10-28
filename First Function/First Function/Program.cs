2e4222e33796d60475dd06537619628244429363
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-28 22:33:12 +0300

    First Function
    C-Sharp Course First Function

---
+++ First Function/First Function/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Function
+{
+    class Program
+    {
+        static void printName ()
+        {
+            Console.WriteLine("My Name Is Mohammed");
+        }    
+        
+        static void Main(string[] args)
+        {
+            printName();
+            Console.ReadKey();
+        }
+    }
+}
