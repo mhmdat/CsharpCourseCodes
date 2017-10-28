917c43fbcc516e865e44df92a2e34c67b89e1673
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-28 10:02:08 +0300

    Challange 27

---
+++ Challanges/Challange 27/Challange 27/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String mystring = "Hello My Dear", mystring2;
+            Console.WriteLine("What Would You Say?");
+
+            mystring = mystring.PadRight(14, 'e');
+             
+            mystring = mystring.PadRight(15, 's');
+            
+            mystring = mystring.PadRight(16, 't');
+            Console.Write($"\n\t{mystring}");
+            Console.ReadKey();
+        }
+    }
+}
