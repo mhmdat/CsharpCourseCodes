fec7c48f143a5741a455e9c685f31b9dd74df6f1
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-28 22:12:28 +0300

    Split Functions With String Type
    C-Sharp Course Split Functions With String Type

---
+++ Split Functions With String Type/Split Functions With String Type/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_Functions_With_String_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring;
+            char[] separator = { ' ', 'u' };
+            mystring = Console.ReadLine();
+            string[] mynewWords;
+            mynewWords = mystring.Split(separator);
+            foreach (string word in mynewWords)
+            {
+                Console.WriteLine($"{word}");
+            }
+            Console.ReadKey();
+           
+
+        }
+    }
+}
