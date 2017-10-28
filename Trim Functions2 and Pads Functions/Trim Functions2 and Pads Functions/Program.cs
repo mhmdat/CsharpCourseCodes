ea0586da3377c8e6a8c1987d448128326c430e07
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-28 09:29:59 +0300

    Trim Fuction2 and Pads Functions
    C-Sharp Course Trim Fuction2 and Pads Functions

---
+++ Trim Functions2 and Pads Functions/Trim Functions2 and Pads Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trim_Functions2_and_Pads_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring, mystring2;
+            char [] mytrimchar = {'h', 'e', 'i', ' '};
+            Console.WriteLine("Please Enter Your String");
+            mystring = Console.ReadLine();
+            mystring = mystring.ToLower();
+            mystring2 = mystring.TrimStart(mytrimchar);
+            Console.WriteLine($"{mystring2}");
+            mystring2 = mystring.TrimEnd(mytrimchar);
+            Console.WriteLine($"{mystring2}");
+            mystring2 = mystring.PadLeft(25, '-');
+            Console.WriteLine($"{mytrimchar}");
+            mystring2 = mystring.PadRight(25, '-');
+            Console.WriteLine($"{mytrimchar}");
+            Console.ReadKey();
+
+        }
+    }
+}
