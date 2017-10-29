31ad2120770e1a119dd4b7e6e1deb2572e849e53
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-30 01:18:32 +0200

    Return Value Functions
    C-Sharp Course Return Value Functions

---
+++ Return Value Functions/Return Value Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Value_Functions
+{
+    class Program
+    {
+        static void addNumbers(int myFirstN, int mySecondN)
+        {
+            int temp = myFirstN + mySecondN;
+            Console.WriteLine($"{temp}");
+        }
+        static int addNumbers2(int myFirst, int mySecond)
+        {
+            int temp = myFirst + mySecond;
+            return temp;
+           
+        }
+        static void Main(string[] args)
+        {
+            int myVar1 = 5, myVar2 = 3;
+            addNumbers(myVar1, myVar2);
+            Console.WriteLine($"{addNumbers2(myVar1, myVar2)}");
+            Console.ReadKey();
+        }
+    }
+}
