93818240337d2ca479ca4e734c2d62e3948fbb76
Mohammad Atiyeh <alhachemm@gmail.com>
2017-10-30 01:45:37 +0200

    Minimum Value Function
    C-Sharp Course Minimum Value Function

---
+++ Minimum Value Function/Minimum Value Function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_Value_Function
+{
+    class Program
+    {
+        static int MinVal(int[] myArray)
+        {
+            int minVal = myArray[0];
+            for(int i = 1; i < myArray.Length; i++)
+            {
+                if (myArray[i] < minVal)
+                    minVal = myArray[i];
+            }
+            return minVal;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 11, 10, 3, 6, 2, 8, 9, 3, 50, 2 };
+            int minVal = MinVal(myArray);
+            Console.WriteLine($"The Minimum Value in My Array is {minVal}");
+            Console.ReadKey();
+        }
+    }
+}
