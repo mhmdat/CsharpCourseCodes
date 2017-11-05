8474206d4f12ccbd58603598f9ccdda6e2913d82
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-05 12:33:12 +0200

    Challange 31

---
+++ Challanges/Challange 31/Challange 31/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange_31
+{
+    class Program
+    {
+        static int MinValue(int[] MyArray2, out int myindex)
+        {
+            int minval = MyArray2[0];
+            myindex = 0;
+            for (int i = 1; i < MyArray2.Length; i++)
+            {
+                if (MyArray2[i] < minval) ;
+                {
+                    minval = MyArray2[i];
+                    myindex = i;
+                }
+            }
+            return minval;
+        }
+        static void Main(string[] args)
+        {
+            int myindex;
+            int[] myarray = {1, 4, 5, 9, 74, 0 };
+            int minval = MinValue(myarray, out myindex);
+            Console.WriteLine($"The Minimum Value In My Array is {minval} and the Index is {myindex}");
+            Console.ReadKey();
+        }
+    }
+}
