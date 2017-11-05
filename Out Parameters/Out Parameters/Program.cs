a9201ab42cec6a407ab427b1df3cc79ca06d1fe3
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-05 12:11:10 +0200

    Out Parameters
    C-Sharp Course Out Parameters

---
+++ Out Parameters/Out Parameters/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_Parameters
+{
+    class Program
+    {
+        static int MinValue(int[] myarray2, out int myindex)
+        {
+            int minval = myarray2[0];
+            myindex = 0;
+            for (int i = 1; i < myarray2.Length; i++)
+            {
+                
+                    if ( myarray2[i] < minval)
+                { 
+                    minval = myarray2[i];
+                    myindex = i;
+                }
+            }
+                return minval;              
+        }
+        static void Main(string[] args)
+        {
+            int myindex;
+            int[] myArray = { 11, 10, 43, 6, 22, 8, 9, 13, 50, 22 };
+            int minVal = MinValue(myArray, out myindex);
+            Console.WriteLine($"The Minimum Value in My Array is {minVal} and the index is {myindex}");
+            Console.ReadKey();
+        }
+    }
+}
