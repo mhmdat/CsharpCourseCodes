aba33288c895555310e9f9772e96c0119ca8d155
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-06 09:00:06 +0200

    Variables Scope
    C-Sharp Course Variable Scope

---
+++ Variables Scope/Variables Scope/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_Scope
+{
+    class Program
+    {
+        static string mystring;
+        static void printmystring()
+        {
+            string mystring = "My String Inside PrintMyString Method";
+            Console.WriteLine($"{mystring}");
+            Console.WriteLine($"{Program.mystring}");
+
+        }
+        static void Main(string[] args)
+        {
+            string mystring = "My String Inside Main Method";
+            Program.mystring = "Global My String Variable";
+            printmystring();
+            Console.WriteLine($"{mystring}");
+            Console.WriteLine($"{Program.mystring}");
+            Console.ReadKey();
+        }
+    }
+}
