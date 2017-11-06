0e411ac4f632b30e952db0761f7824c510045783
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-06 09:10:23 +0200

    Variable Scope In Structure
    C-Sharp Course Variable Scope In Structure

---
+++ Variable Scope In Structure/Variable Scope In Structure/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_Scope_In_Structure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, index = 0;
+            for (i = 0; i < 10; i++)
+            {
+                index = i;
+                Console.WriteLine($"{index}");
+            }
+            Console.WriteLine($"The Last Index Output In Loop : {index}");
+            Console.ReadKey();
+        }
+    }
+}
