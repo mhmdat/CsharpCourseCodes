79b22efbc8c8afb561379492efe8305def633084
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-07 14:48:04 +0200

    Structure Function
    C-Sharp Course Struct. Function

---
+++ Struct Function/Struct Function/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Function
+{
+    class Program
+    {
+        struct fullName
+        {
+            public string myfirstname, mylastname;
+            public string printname() => myfirstname + " " + mylastname; 
+        }
+        static void Main(string[] args)
+        {
+            fullName myfullname;
+            myfullname.myfirstname = "Mhmd";
+            myfullname.mylastname = "Atiyeh";
+            //Console.Write($"{myfullname.myfirstname} {myfullname.mylastname}");
+            Console.Write($"{myfullname.printname()}");
+            Console.ReadKey();
+        }
+    }
+}
