15b08a78e320b6c42390d760dadc64c25d46e81d
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-07 14:22:15 +0200

    The Main Method
    C-Sharp Course The Main Method

---
+++ The Main Method/The Main Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Main_Method
+{
+    class Program
+    {
+        static int Main(string[] args)
+            
+        {
+
+            Console.WriteLine($"{args.Length} Command Line arguments Has Entered :");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
