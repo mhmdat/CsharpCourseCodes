b6f1dd53601c5ea66e74b96f09280282eed2ea5f
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-08 10:29:04 +0200

    Overloading
    C-Sharp Course Overloading

---
+++ Overloading/Overloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        public static int printNumber(int mynumber)
+        {
+            return mynumber;
+        }
+        public static double printNumber(double mynumber)
+        {
+            return mynumber;
+        }
+        static void Main(string[] args)
+        {
+            int mynumber = 9;
+            Console.Write($"{printNumber(mynumber)}");
+            Console.ReadKey();
+        }
+    }
+}
