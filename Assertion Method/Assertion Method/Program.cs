c7baca6998963b86c5d7e6c0b2ebbf231bbff9a1
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-17 13:23:06 +0200

    Assertion Methods
    C-Sharp Course Assertion Methods

---
+++ Assertion Method/Assertion Method/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Diagnostics;
+namespace Assertion_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter Your Value");
+            int i = Convert.ToInt32(ReadLine());
+            if (i >= 100)
+            {
+                WriteLine($"{i}");
+            }
+            else
+            {
+                Debug.WriteLine("We Are Here");
+                //Debug.Assert(i >= 100, "short", "long");
+                Trace.Assert(i>=100,"short","long");
+            }
+            WriteLine("Done");
+            ReadKey();
+        }
+    }
+}
