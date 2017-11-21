0e569b0d56f5d0b6c9640daa56f4d725fcc4ec68
Mohammad Atiyeh <alhachemm@gmail.com>
2017-11-21 13:13:37 +0200

    Multi casting Delegate
    C_Sharp Multi casting Delegate

---
+++ Multicasting Delegate/Multicasting Delegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multicasting_Delegate
+{
+    delegate int MyNewNumber(int myNumber);
+
+    class Program
+    {
+        static int myXNumber = 3;
+        static int addfunc(int number)
+        {
+            myXNumber += number;
+            return myXNumber;
+        }
+        static int mulfunc(int number)
+        {
+            myXNumber *= number;
+            return myXNumber;
+        }
+        static void Main(string[] args)
+        {
+            MyNewNumber n;
+            MyNewNumber n1 = new MyNewNumber(addfunc);
+            MyNewNumber n2 = new MyNewNumber(mulfunc);
+            n1(4);
+            Console.Write($"{myXNumber} \n");
+            n2(5);
+            Console.Write($"{myXNumber} \n");
+            n = n1 + n2;
+            n(5);
+            Console.Write($"{myXNumber} \n");
+            Console.ReadKey();
+
+                
+        }
+    }
+}
