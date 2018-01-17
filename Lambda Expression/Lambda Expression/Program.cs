d2cf7db4fefca2cbda4c9b268642d74b334e9ff7
Mohammad Atiyeh <alhachemm@gmail.com>
2018-01-17 08:25:46 +0200

    Lambda Expression
    C-Sharp Course Lambda Expression

---
+++ Lambda Expression/Lambda Expression/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Lambda_Expression
+{
+    delegate int MyNewNumber(int MyNumber);
+
+    class Program
+    {
+        static int MyNumber = 3;
+        static void Main(string[] args)
+        {
+
+            //MyNewNumber n = X => X + MyNumber;
+            //MyNewNumber n1 = X => X * MyNumber;
+            //Write($"MyXNumber = {n(3)}\n");
+            //Write($"MyXNumber = {n1(4)}\n");
+            //ReadKey();
+            Func<int, bool> func = (x => x % 2 != 0);
+            int[] myArray = { 1, 2, 3, 4, 5, 6 };
+            for (int i = 0; i < myArray.Length; i++)
+                WriteLine(func.Invoke(myArray[i]));
+            ReadKey();
+        } 
+    }
+}
