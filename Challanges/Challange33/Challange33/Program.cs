99b504dba61b9cb2d92b6abcc9e27aca7a8b69fd
Mohammad Atiyeh <alhachemm@gmail.com>
2018-01-17 08:43:29 +0200

    Challange33
    C-Sharp Course Challange33

---
+++ Challanges/Challange33/Challange33/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Challange33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, bool> func2 = (X => X > 10);
+            int[] myarray = { 2, 3, 6, 11, 5, 12, 15, 7 };
+            for (int i = 0; i < myarray.Length; i++)
+                WriteLine(func2.Invoke(myarray[i]));
+            ReadKey();
+        }
+    }
+}
