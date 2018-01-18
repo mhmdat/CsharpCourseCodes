77e74fb44e58c7ee2c69d7b55e9bc2bd7c6cd262
Mohammad Atiyeh <alhachemm@gmail.com>
2018-01-18 07:15:30 +0200

    NullableValues
    C-Sharp Course NullableValues

---
+++ NullableValues/NullableValues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myvariable = null;
+            int? myNumber = null;
+            double? myDouble = null;
+            Console.WriteLine($"{myvariable}");
+            Console.WriteLine($"{myNumber}");
+            Console.WriteLine($"{myDouble}");
+            Console.ReadKey();
+        }
+    }
+}
