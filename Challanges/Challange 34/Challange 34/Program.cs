fbcc9aed171f4be69a79be3f589ad084c5f08b23
Mohammad Atiyeh <alhachemm@gmail.com>
2018-01-20 09:20:54 +0200

    Challange 34
    C-Sharp Course Challange 34

---
+++ Challanges/Challange 34/Challange 34/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange_34
+{
+    class Program
+    {
+        static int MyNum(params int[] value)
+        {
+            Console.WriteLine("You sent me {0} Numbers", value.Length);
+            int maxnum = 0;
+            if (value.Length == 0)
+                return maxnum;
+            for (int i = 0; i < value.Length; i++)
+                maxnum = value.Max();
+            return maxnum;
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int max;
+            max = MyNum ( 16, 17, 20, 25, 77, 123);
+            Console.WriteLine("Max number is :{0}", max);
+            Console.ReadKey();
+
+        }
+    }
+}
