63441c2be58f9018f62cb2210ebb602c3ea9467d
Mohammad Atiyeh <alhachemm@gmail.com>
2018-01-22 10:01:03 +0200

    GoTo
    C-Sharp Course GoTo

---
+++ GoTo/GoTo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace GoTo
+{
+    class Program
+    {
+        static int myfunction()
+        {
+            int counter = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (j == 5)
+                    {
+                        goto MyLable;
+                    }
+                    counter++;
+                }
+                MyLable:
+                continue;
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            //WriteLine(myfunction());
+            int counter = 0;
+            myLabel:
+            if (counter < 10)
+            {
+                counter++;
+                goto myLabel;
+            }
+            else WriteLine("Done");
+            WriteLine(counter);
+            ReadKey();
+
+        }
+    }
+}
