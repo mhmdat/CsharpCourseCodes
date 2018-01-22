b954f3b2923fea90cb0f2f0b1ff3b25f1175060b
Mohammad Atiyeh <alhachemm@gmail.com>
2018-01-22 10:18:23 +0200

    RecursiveFunction
    C-Sharp Course RecursiveFunction

---
+++ Recursive Function/Recursive Function/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Function
+{
+    class Program
+    {
+        static int MyRecursiveFunction(int myvalue, ref int counter)
+        {
+            counter++;
+            //if value = 0.
+            if (myvalue == 0)
+                return 0;
+            //if va;ue less than 10.
+            else if(myvalue < 10)
+            {
+                return MyRecursiveFunction(myvalue + 1, ref counter);
+
+            }
+
+            //if value equal or exceeds 10.
+            return myvalue;
+                
+        }
+        static void Main(string[] args)
+        {
+            int counter = 0, myresult;
+            myresult = MyRecursiveFunction(4, ref counter);
+            Console.WriteLine($"My result is {myresult} and counter value is {counter}");
+            Console.ReadKey();
+        }
+    }
+}
