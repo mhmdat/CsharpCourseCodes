e90cbbbd9b44b04f947cc4a153ddd5940d365e60
Mohammad Atiyeh <alhachemm@gmail.com>
2018-01-23 11:21:47 +0200

    Optional Parameters
    C-Sharp Course Optional Parameters

---
+++ Optional Parameters/Optional Parameters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    class Program
+    {
+        public static void printvalues(string name = "Mhmd Atiyeh", int myvalue = 7)
+        {
+            Console.WriteLine($"Name {name} Number {myvalue}");
+        }
+        static void Main(string[] args)
+        {
+            string name = "yusif";
+            int value = 15;
+            printvalues();
+            printvalues(name);
+            printvalues(name, value);
+            printvalues(name: "Kamel", myvalue: 6);
+            Console.ReadKey();
+        }
+    }
+}
